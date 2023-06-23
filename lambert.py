094bd7fd060f3f1de9182bff498eeca4915644c6
CalcuLector <137517171+VolodymyrGandzhuk@users.noreply.github.com>
2023-06-24 00:34:02 +0200

    Add files via upload

--- lambert.py
+++ lambert.py
@@ -1,6 +1,7 @@
 # Programa para determinar la función W de Lambert
 
 from math import exp
+import sys
 
 def lambert(x):
   l = 0
@@ -10,9 +11,9 @@ def lambert(x):
     r =  (l * exp(l) - x) / ((1 + l) * exp(l))
   return l
 
-a = float(input("Argumento: "))
-if a<-exp(-1):
+a = float(sys.argv[1])
+if  < -exp(-1):
   print("Error: no se puede calcular la función W de Lambert")
 else:
   w = round(lambert(a), 8)
-  print(w)
+  print(w) 
