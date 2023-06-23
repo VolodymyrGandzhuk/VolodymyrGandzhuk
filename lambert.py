51ec313507b16293a7dd74bc8ce567958bd69b18
CalcuLector <137517171+VolodymyrGandzhuk@users.noreply.github.com>
2023-06-24 00:13:54 +0200

    Programa para determinar la función W de Lambert

---
+++ lambert.py
@@ -0,0 +1,18 @@
+# Programa para determinar la función W de Lambert
+
+from math import exp
+
+def lambert(x):
+  l = 0
+  r = (l * exp(l) - x) / ((1 + l) * exp(l))
+  while abs(r) > 10 ** -8:
+    l -= r
+    r =  (l * exp(l) - x) / ((1 + l) * exp(l))
+  return l
+
+a = float(input("Argumento: "))
+if a<-exp(-1):
+  print("Error: no se puede calcular la función W de Lambert")
+else:
+  w = round(lambert(a), 8)
+  print(w)
